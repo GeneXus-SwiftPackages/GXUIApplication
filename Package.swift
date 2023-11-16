ba1b55d04460039795997b32649508293a393605
genexusbot <builder@genexus.com>
2023-11-16 11:16:08 -0300

    Package version: 1.3.0-beta.8

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUIApplicationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.3.0-beta.7")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.3.0-beta.8")
 	],
 	targets: [
 		.target(name: "GXUIApplicationWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUIApplication",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUIApplication-1.3.0-beta.7.xcframework.zip",
-			checksum: "070288ce142e425573184c40b275b768c9ceed7c2d47718da713699dbd24e603"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUIApplication-1.3.0-beta.8.xcframework.zip",
+			checksum: "1199d58826e76406050426c0b40e3c548669e251e453886971213d7d8fe63745"
 		)
 	]
 )
