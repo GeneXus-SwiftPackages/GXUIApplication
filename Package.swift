058e9e0a6a12ea07450f088b232469ea1bd5aaa4
genexusbot <builder@genexus.com>
2024-02-19 22:52:27 -0300

    Package version: 1.5.0-beta.5

--- Package.swift
+++ Package.swift
@@ -3,26 +3,26 @@ import PackageDescription
 
 let package = Package(
 	name: "GXUIApplication",
-	platforms: [.iOS("12.0"), .tvOS("12.0"), .visionOS("1.0")],
+	platforms: [.iOS("12.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "GXUIApplication",
 			targets: ["GXUIApplicationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-beta.4")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-beta.5")
 	],
 	targets: [
 		.target(name: "GXUIApplicationWrapper",
 				dependencies: [
 					"GXUIApplication",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS, .visionOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUIApplication",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUIApplication-1.5.0-beta.4.xcframework.zip",
-			checksum: "d4ae18317c5965d632731ce1389fa932085cad9903168d1891389876ee333865"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUIApplication-1.5.0-beta.5.xcframework.zip",
+			checksum: "75825c30b9198507c29b007c6d7a94fd3b9846f36f940ce5862b734f9f2410c7"
 		)
 	]
 )
